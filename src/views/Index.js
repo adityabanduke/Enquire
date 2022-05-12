19cb3e22cdb7f29857852e465f7508e1e61c8fa6
Aryaman Swami <swamiaryaman21.as@gmail.com>
2022-05-12 22:28:39 +0530

    aryaman commit's

--- src/views/Index.js
+++ src/views/Index.js
@@ -54,7 +54,6 @@ import {
     
   
   } from "reactstrap";
-  import {Link} from 'react';
  import "dist/css/style.scss";
  import logoLight from "../dist/images/logo-light.svg";
 import illustration from "../dist/images/header-illustration-light.svg";
@@ -68,7 +67,7 @@ import feature02 from "../dist/images/feature-02-light.svg";
 
 import feature03 from "../dist/images/feature-03-light.svg";
 
-
+import { Link } from 'react-router-dom'
 
 
 
@@ -82,7 +81,7 @@ const Index = (props) => {
   <div className="body-wrap boxed-container">
       <header className="site-header">
           <div className="container">
-          <nav class="navbar">
+          <nav className="navbar">
           <div className="site-header-inner">
                   <div className="brand header-brand">
                       <h1 className="m-0">
@@ -108,13 +107,13 @@ const Index = (props) => {
 
                   
                 </ul>
-                <Link to="views/userLogin.js"> <Button className="my-2 nav-button button-text" color="primary" type="button">
+                <Link to='views/userLogin'><Button className="my-2 nav-button button-text" color="primary" type="button">
                   Login
                 </Button></Link>
                
-                {/* <Button className="my-2 nav-button button-text" color="primary" href="views/userRegister.js" type="button">
+                <Button className="my-2 nav-button button-text" color="primary" href="views/userRegister.js" type="button">
                   Sign Up
-                </Button> */}
+                </Button>
                 </nav>
            
           </div>
