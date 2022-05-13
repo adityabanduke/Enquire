231df1486017931c8f15d1a43d3906e824b23637
dhruv360code <dhruvtiwari003@gmail.com>
2022-05-13 10:12:41 +0530

    installed material packages

--- src/views/Index.js
+++ src/views/Index.js
@@ -113,10 +113,11 @@ const Index = (props) => {
                   Login
                 </Button>
                 </Link>
-               
-                <Button className="my-2 nav-button button-text" color="primary" href="views/userRegister.js" type="button">
+                <Link to="/register">
+                <Button className="my-2 nav-button button-text" color="primary"  type="button">
                   Sign Up
                 </Button>
+                </Link>
                 </nav>
             
           </div>
