6e01a806631829bf1b1bffbb18cd90812a9e17d5
Aryaman Swami <swamiaryaman21.as@gmail.com>
2022-05-15 19:35:09 +0530

    Queue Management

--- src/views/user/HospitalDetail.js
+++ src/views/user/HospitalDetail.js
@@ -13,44 +13,106 @@ import {
 } from "reactstrap";
 
 import { db } from '../../config/firebase-enquire';
+import firebase from '../../config/firebase-enquire';
 import { useHistory, useParams } from 'react-router-dom'
 
 
 export default class HospitalDetails extends Component {
 
     constructor(props) {
-        
+
+        var today = new Date(),
+        todayDate =
+          today.getFullYear() +
+          "-" +
+          ((today.getMonth() + 1 < 10 ? "0" : "") + (today.getMonth() + 1)) +
+          "-" +
+          ((today.getDate() < 10 ? "0" : "") + today.getDate());
+      var time = today.getHours() + ":" + today.getMinutes();
+
         super(props)
         this.state = {
             h_id: "",
+            user_id:"",
+            today:todayDate,
+            time:time,
+
         }
+        this.bookAppointment = this.bookAppointment.bind(this);
     }
 
     componentDidMount() {
         // var currenturl = window.location.pathname;
-               // this.setState({ h_id: h_id });
-               var currenturl = window.location.search;
-               var currenturlsearch = new URLSearchParams(currenturl);
-               var h_id = currenturlsearch.get("h_id");
-               console.log(h_id)
+        // this.setState({ h_id: h_id });
 
-this.setState({h_id : h_id});
+        firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				firebase.database().ref("users/" + user.uid).once('value').then((snapshot) => {
+					var Data = snapshot.val();
+					console.log(Data);
+                    this.setState({user_id: Data.user_uid});
 
-  db.collection('Admin').doc(h_id).get().then((snapshot)=>{
-  var hData = snapshot.data();
-  console.log(hData);
+		
+        var currenturl = window.location.search;
+        var currenturlsearch = new URLSearchParams(currenturl);
+        var h_id = currenturlsearch.get("h_id");
+        console.log(h_id)
 
-  this.setState({hData : hData , users: hData.patients});
-  })
-        
+        this.setState({ h_id: h_id });
+
+
+     
+
+   
+
+
+
+        db.collection("Admin").doc(h_id).get().then((snapshot) => {
+            console.log(snapshot.data());
+            var hospitalData = snapshot.data();
+            console.log(hospitalData.name);
+            this.setState({ hData: hospitalData });
+
+            // console.log(userData);
+        }).then((err) => {
+            if(err){
+                console.log(err);
+            }else{
+                console.log("Success!!!")
+            }
+        })
+
+
+    })
+}
+else {
+    window.location.href = "/login";
+}
+})
 
     }
 
+    bookAppointment = () => {
+        // db.collection('Admin').doc(this.state.h_id).update({
+        //     users: db.FieldValue.arrayUnion(this.state.userData)
+        // });
+       
 
-    bookAppointment= ()=>{
-        db.collection('Admin').doc(this.state.h_id).add().then((snapshot)=>{
-           
-            })
+
+        firebase.database().ref("Hospitals/"+this.state.h_id +"/"+this.state.user_id).set({
+            bookingDate: this.state.today,
+            bookingTime:this.state.time,
+
+        }).then(()=>{
+
+            firebase.database().ref("users/"+this.state.user_id + "/bookings/"+ this.state.h_id).set({
+                bookingDate: this.state.today,
+                bookingTime:this.state.time,            })
+        }).then(()=>{
+            alert("Appointment Booked Successfully");
+        })
+
+      
     }
     render() {
         return (
@@ -70,10 +132,10 @@ this.setState({h_id : h_id});
                     <Container className="d-flex align-items-center" fluid>
                         <Row>
                             <Col lg="8" md="10">
-                                {this.state.hData? <h1 className="display-2 text-white">{this.state.hData.name}</h1>:null}
-                                {this.state.hData?<p className="text-white mt-0 mb-5">
-              {this.state.hData.about}
-            </p>:null}
+                                {this.state.hData ? <h1 className="display-2 text-white">{this.state.hData.name}</h1> : null}
+                                {this.state.hData ? <p className="text-white mt-0 mb-5">
+                                    {this.state.hData.about}
+                                </p> : null}
 
                             </Col>
                         </Row>
@@ -81,9 +143,9 @@ this.setState({h_id : h_id});
                 </div>
                 <Container className='text-center' fluid>
 
-                <Button className='text-center my-4' color="info" onClick={this.bookAppointment}>
-                            Book Appointment
-                        </Button>
+                    <Button className='text-center my-4' color="info" onClick={this.bookAppointment}>
+                        Book Appointment
+                    </Button>
                 </Container>
             </>
         )
