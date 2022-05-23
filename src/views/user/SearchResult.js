3f1bc0f50f0e0f717cb8a3a5d05cdd8c150564f0
dhruv360code <dhruvtiwari003@gmail.com>
2022-05-24 03:47:25 +0530

    geo location added

--- src/views/user/SearchResult.js
+++ src/views/user/SearchResult.js
@@ -10,15 +10,19 @@ import FolderIcon from '@mui/icons-material/Folder';
 import PageviewIcon from '@mui/icons-material/Pageview';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import { db } from '../../config/firebase-enquire';
-
+import * as ttapi from '@tomtom-international/web-sdk-services';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import Typography from '@mui/material/Typography';
 import "../../assets/css/card.css";
 import Loader from "../../components/loader/Loader.js";
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+
 
 
 
@@ -30,14 +34,24 @@ export default class SearchResult extends Component {
 			myOptions: [],
 			hospitalData: [],
 			loading:false,
+			userLongitude:23.11,
+			userLatitude:77.94,
+			dandt:[],
+			destinations: [],
 
 		}
 
 		this.getDataFromAPI = this.getDataFromAPI.bind(this);
 		this.handleKeyPress = this.handleKeyPress.bind(this);
+		this.getInfo = this.getInfo.bind(this);
+
 
 
 	}
+
+	
+
+	
 
 	componentDidMount() {
 
@@ -56,6 +70,10 @@ export default class SearchResult extends Component {
 				firebase.database().ref("users/" + user.uid).once('value').then((snapshot) => {
 					var userData = snapshot.val();
 					console.log(userData);
+					this.setState({userLatitude:userData.latitude});
+					this.setState({userLongitude:userData.longitude});
+
+
 
 				})
 
@@ -66,6 +84,49 @@ export default class SearchResult extends Component {
 			}
 		})
 	}
+
+	    
+    getInfo = (destination) => {
+
+		const callParameters = {
+								key: "CE2fnCEGHeTVTeEERrtbhIeVXn7mPsnI",
+								destinations: destination,
+								origins: [{
+									point: {
+									  latitude: this.state.userLatitude,
+									  longitude: this.state.userLongitude,
+									}
+								  }]
+							  }
+					
+		  return new Promise((resolve, reject) => {
+			ttapi.services
+			  .matrixRouting(callParameters).then((matrixApIResults) => {
+				const results = matrixApIResults.matrix[0];
+				console.log(results);
+				 const resultsArray = results.map((result, index) => {
+					console.log(result);
+
+
+				  return {
+					// location: locations[index],
+					drivingtime: result.response.routeSummary.travelTimeInSeconds,
+					drivingdistance: result.response.routeSummary.lengthInMeters,
+
+				  }
+				})
+				console.log("hi how are you");
+
+				this.setState({dandt:resultsArray});
+				console.log(this.state.dandt);
+
+			  })
+		  })
+		 
+	
+}
+	
+
 
 	handleKeyPress = (e) => {
 
@@ -89,6 +150,33 @@ export default class SearchResult extends Component {
 								tempData.push(doc.data());
 
 							})
+
+							
+                            
+						
+                            var arr = [];
+							for( let i = 0; i < tempData.length; i++){
+								arr.push({
+									point: {
+									  latitude: tempData[i].latitude,
+									  longitude: tempData[i].longitude,
+									}
+								  })
+							}
+
+							this.setState({destinations:arr});
+							console.log(this.state.destinations);
+
+							console.log(this.state.userLatitude);
+							console.log(this.state.userLongitude);
+
+							
+
+							this.getInfo(this.state.destinations);
+							 
+						
+						
+
 							this.setState({ hospitalData: tempData });
 							console.log(this.state.hospitalData);
 							// localStorage.setItem('hospitalData', JSON.stringify(this.state.hospitalData));
@@ -247,21 +335,49 @@ export default class SearchResult extends Component {
 		
 				<Container className='mt-4 mx-auto' >
 
-					{this.state.loading ? this.state.hospitalData.map((hospital) => (
+					 {this.state.loading  ? ( this.state.dandt.length ?  this.state.hospitalData.map((hospital, index) => (
 						<>
-		<div className="container mt-5 mb-5" >
+{console.log(hospital)}
+{console.log(this.state.dandt)}
+
+		<div key={index} className="container mt-5 mb-5" >
 		<div className="d-flex justify-content-center row" >
 			<div className="col-md-10">
-				<div className="row p-2 bg-white border rounded" style={{ 'box-shadow': '0px 0px 6px 1px rgba(0,0,0,0.47)' }}>
+				<div className="row p-2 bg-white border rounded" style={{ 'boxShadow': '0px 0px 6px 1px rgba(0,0,0,0.47)' }}>
 					<div className="col-md-3 mt-1"><img className="img-fluid img-responsive rounded product-image" src={hospital.imageAsUrl ? hospital.imageAsUrl : "https://www.blkmaxhospital.com/slider-mobile.jpg"}/></div>
 					<div className="col-md-6 mt-1">
 						<h3>	{hospital.name}</h3>
 						<div className="d-flex flex-row">
 							<div className="ratings mr-2"><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /><i className="fa fa-star" /></div>
 						</div>
+						<div style={{width:'100%',backgroundColor:'#f5f6fa', borderRadius:'8px'}}className="d-flex  justify-content-around align-items-center p-1 mt-1 flex-row">
+				<span className='d-flex'>
+				<DirectionsCarIcon></DirectionsCarIcon>
+				
+				<h5 className='mt-1'>
+					Driving Distance : {((this.state.dandt[index].drivingdistance)/1000).toPrecision(2)} KM
+				</h5>
+				</span>
+			   
+			
+
+				
+				<span className='d-flex'>
+				<AccessTimeFilledIcon></AccessTimeFilledIcon>
+				<h5 className='mt-1'>
+					Driving Time : {(this.state.dandt[index].drivingtime/60).toPrecision(2)} Mins
+				</h5>
+				</span>
+			   
+			
+
+				
+                  
+						</div>
+
 												<p className="text-justify text-truncate  para mb-0">	{hospital.address}<br /><br /></p>
 					</div>
-					<div className="align-items-center align-content-center col-md-3 border-left mt-1">
+					<div className="align-items-center align-content-center col-md-3 border-left mt-2">
 					
 						{console.log(hospital.avalabilityStatus)}
 						{hospital.avalabilityStatus?<h5 className="text-success">Available</h5>:<h5 className="text-danger"> Not Available</h5>}
@@ -285,7 +401,7 @@ export default class SearchResult extends Component {
 	</div>
 </>
 
-					)) : <Loader/>}
+					)) :<Loader/> ) : <Loader/>}
 
 
 
